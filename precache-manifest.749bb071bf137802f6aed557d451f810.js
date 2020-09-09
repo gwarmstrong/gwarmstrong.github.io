5dc48f3403272179fa6254afb245207e11ef443f
gwarmstrong <garmstrong@colgate.edu>
2020-09-09 15:34:37 -0700

    Updates

---
+++ precache-manifest.749bb071bf137802f6aed557d451f810.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e46d3c39463595ad82c67bd20ae2fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdd6261bd3d55fdb3ba7",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "52301dbd5201d082c49a",
+    "url": "/static/css/main.81b60816.chunk.css"
+  },
+  {
+    "revision": "bdd6261bd3d55fdb3ba7",
+    "url": "/static/js/2.8f9c4004.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8f9c4004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52301dbd5201d082c49a",
+    "url": "/static/js/main.f74f2357.chunk.js"
+  },
+  {
+    "revision": "d248a010eb0d00692df9",
+    "url": "/static/js/runtime-main.ce9299b5.js"
+  },
+  {
+    "revision": "86500e69152bb6d6b9aecbe9dbbafa8d",
+    "url": "/static/media/colgate-logo-1.86500e69.png"
+  },
+  {
+    "revision": "69b5065d6e0c2fc39952e226e3e87d28",
+    "url": "/static/media/me-photo.69b5065d.jpg"
+  },
+  {
+    "revision": "b619b9512233854a80fd5041cad58aa0",
+    "url": "/static/media/ucsd-logo.b619b951.png"
+  }
+]);
